828745575b792050c071dd13a12cba45b1bd4b82
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-03-11 10:21:41 +0545

    Dynamic movements UI printed

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
@@ -22,3 +24,44 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+const accountOne = {
+  owner: 'Gorakh Raj Joshi',
+  movements: [200, -300, 400, 500, -700, 1300, -1100, 1600],
+  interestRate: 1.5,
+  pin: 1234,
+};
+
+const accountTwo = {
+  movements: [1000, -600, 700, -300, 1300, -1100, 1600],
+  owner: 'Shankar Shrestha',
+  interestRate: 1.1,
+  pin: 2222,
+};
+
+const accounts = [accountOne, accountTwo];
+
+const [{ movements }] = accounts;
+
+// < 0 => withdrawal
+// > 0 => deposit
+
+movements.forEach(function (mov, i) {
+  // const type = if (mov > 0) {
+  //   console.log('deposit');
+  // } else {
+  //   console.log('withdrawal');
+  // }
+
+  const type = mov > 0 ? 'deposite' : 'withdrawal';
+
+  const html = `<div class="movements__row">
+          <div class="movements__type movements__type--${type}">
+            ${i + 1} ${type}
+          </div>
+          <div class="movements__value">${mov}€</div>
+        </div>`;
+  containerMovements.insertAdjacentHTML('afterbegin', html);
+});
+
+console.dir(containerMovements);
